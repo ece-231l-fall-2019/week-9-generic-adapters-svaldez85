600b1061cd54f2b741d2b00d463c182f7812ab1d
svaldez85 <54284663+svaldez85@users.noreply.github.com>
2019-10-21 19:26:48 -0600

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,2 +1,22 @@
 #include "List.h"
 #include <cstdef>
+
+template<typename T>
+Queue
+{
+  protected:
+     List<T> c;
+  public:
+     T& front();
+     T& back();
+     const T& front() const;
+     const T& back() const;
+     void push(const T&);
+     void pop();
+     size_t size() const;
+     bool empty() const;
+     Queue<T>& operator=(const Queue<T>&);
+};
+
+bool operator==(const Queue<T>&, const Queue<T>&);
+bool operator!=(const Queue<T>&, const Queue<T>&);
