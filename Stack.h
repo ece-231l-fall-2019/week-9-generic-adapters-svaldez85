fef0c35689860ed7b1dc97e1bbb519e4ec85222e
svaldez85 <54284663+svaldez85@users.noreply.github.com>
2019-10-21 19:27:17 -0600

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,1 +1,19 @@
 #include "List.h"
+
+template<typename T>
+Stack
+{
+  protected:
+     List<T> c;
+  public:
+     T& top();
+     const T& top() const;
+     void push(const T&);
+     void pop();
+     size_t size() const;
+     bool empty() const;
+     Stack<T>& operator=(const Stack<T>&);
+};
+
+bool operator==(const Stack<T>&, const Stack<T>&);
+bool operator!=(const Stack<T>&, const Stack<T>&);
